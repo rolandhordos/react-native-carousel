3c9965c0a8d0f9c20abeb82f65df88dbde2bfe55
Roland Hordos <rolandhordos@me.com>
2017-10-25 21:29:02 -0700

    Jest running again with new structure. Simple render test configures `animate` prop to false, for simple static render which passes.

--- __tests__/Carousel.js
+++ __tests__/Carousel.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import Carousel from '../Carousel'
+import Carousel from '../src/Carousel'
 import renderer from 'react-test-renderer'
 
 describe('Simple Carousel', () => {
 
   const carousel = renderer.create(
-    <Carousel width={250}>
+    <Carousel width={250} animate={false}>
       <View><Text>Slide 1</Text></View>
       <View><Text>Slide 2</Text></View>
       <View><Text>Slide 3</Text></View>
